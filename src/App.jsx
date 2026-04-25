cd505b80c4ff913428185ed2d3acfbe4258e5363
chamuditha-t <chamudithatheekshana6815@gmail.com>
2026-04-25 08:56:36 +0530

    feat: implement ScrollToTop component and initialize new app structure with assets

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import ScrollToTop from "./components/ScrollToTop";
 import Home from "./pages/index"; // ඔයාගේ ලොකු ChamuBuilds code එක තියෙන්නේ මෙතන
 import Projects from "./pages/Projects";
 import ProjectDetail from "./pages/ProjectDetail";
@@ -8,6 +9,7 @@ import Message from "./pages/Message";
 export default function App() {
   return (
     <Router>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/projects" element={<Projects />} />
